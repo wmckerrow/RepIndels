8f994354956b9a53d2274a6f4b709c907016e2f5
Wilson McKerrow <wilson_mckerrow@brown.edu>
2018-05-04 09:54:36 -0400

    PEhmm to PEhmm2

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 import numpy
 from distutils.core import setup, Extension
 
-setup(name='PEhmm.c',version=1.0,ext_modules=[Extension('PEhmm', ['PEhmm.c'],include_dirs=[numpy.get_include()])])
+setup(name='PEhmm2.c',version=1.0,ext_modules=[Extension('PEhmm2', ['PEhmm2.c'],include_dirs=[numpy.get_include()])])
